befd623e142245e941acf71cc75bd864fd25c96b
BryanVe <bryan.ve.bv@gmail.com>
2024-08-20 18:58:00 -0500

    feat: configure express with the base route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('initial configuration')
+import express from 'express'
+import { keys } from './config'
+
+const app = express()
+
+app.get('/', (_, res) =>
+	res.json({
+		success: true,
+		message: 'Wallet wise service is running',
+	})
+)
+
+app.listen(keys.PORT, () => {
+	console.log(`Server is running at http://localhost:${keys.PORT}`)
+})
